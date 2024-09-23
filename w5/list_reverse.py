2506b859f7f1ae78e54551a069dc3b7fa3b9f136
akhil-varsh <raisaarmahadevan@gmail.com>
2024-09-24 00:20:02 +0530

    week five

---
+++ w5/list_reverse.py
@@ -0,0 +1,8 @@
+def reverse(lst):
+    return lst[::-1]
+
+# Test the function
+original_list = [1, 2, 3, 4, 5]
+reversed_list = reverse(original_list)
+print(f"Original list: {original_list}")
+print(f"Reversed list: {reversed_list}")
---
+++ w5/matrix_print.py
@@ -0,0 +1,13 @@
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+# Define a matrix
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print("Matrix:")
+print_matrix(matrix)
